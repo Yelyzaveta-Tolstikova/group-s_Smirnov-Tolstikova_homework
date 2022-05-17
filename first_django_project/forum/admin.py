ebef65f4669ff6ca39466bc99db03535f140d448
HeorhiiSmirnov <103209814+HeorhiiSmirnov@users.noreply.github.com>
2022-05-17 18:26:17 +0300

    Delete first_django_project directory

--- first_django_project/forum/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from forum.Models import User, Article, Comment
-
-admin.site.register(User, Article, Comment)
-# admin.site.register(Article)
-# admin.site.register(Comment)
