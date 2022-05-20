922eb57ee1a6343fc4ca0c14399b3b046d3022ce
Alex Botte <votetoB@gmail.com>
2022-05-20 19:55:19 +0300

    Update user.py

--- likeMedium/blog/models/user.py
+++ likeMedium/blog/models/user.py
@@ -1,14 +1,18 @@
 from django.db import models
 import uuid
+from common.models.BaseModel import BaseModel
+from django.contrib.auth.models import PermissionMixin, AbstractBaseUser
 
-class User(models.Model):
+
+class User(AbstractUser, BaseModel):
     nickname = models.CharField(max_length=20)
-    creation_date = models.DateTimeField()
     rating = models.PositiveSmallIntegerField(null=True, blank=True)
     articles_number = models.IntegerField(null=True,blank=True)
     comments_number = models.IntegerField(null=True, blank=True)
     avatar = models.ImageField(null=True, blank=True)
-    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
 
     def __str__(self):
         return self.nickname
+    
+    class Meta:
+        swappable = 'AUTH_USER_MODEL'
